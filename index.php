2bdd4638fb00acc698f3bdf5cabc851a8d0053e3
Ole Bjørn Michelsen <ole@michelsen.dk>
2014-06-24 13:29:01 +0200

    Formatting.

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
-$s=""; //generate a random string of 108KB and a random filename
-$fname = chr(rand(0,57)+65).chr(rand(0,57)+65).chr(rand(0,57)+65).chr(rand(0,57)+65).'.txt';
-for($i=0;$i<108000;$i++)
+// generate a random string of 108KB and a random filename
+$s="";
+$fname =
+	 chr(rand(0,57)+65)
+	.chr(rand(0,57)+65)
+	.chr(rand(0,57)+65)
+	.chr(rand(0,57)+65)
+	.'.txt';
+for($i = 0; $i < 108000; $i++)
 {
-	$n=rand(0,57)+65;
+	$n = rand(0,57)+65;
 	$s = $s.chr($n);
 }
-
-//write s to a file
-file_put_contents($fname,$s);
+file_put_contents($fname, $s);
 $result = file_get_contents($fname);
 echo $result;
